ab12275e2c4e34fa5cf569e855916f5f651046be
TimSpakowski <119943071+TimSpakowski@users.noreply.github.com>
2023-01-23 22:40:44 +0100

    Update Sieb.c

--- Sieb.c
+++ Sieb.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 
 int main() {
-  int prim[30000];
+  int prim[3000000];
   int i, j;
   
-  for (i = 2; i <= 30000; i++) {
+  for (i = 2; i <= 3000000; i++) {
     prim[i] = 1;
   }
   
-  for (i = 2; i <= 30000; i++) {
+  for (i = 2; i <= 3000000; i++) {
     if (prim[i] == 1) {
-      for (j = 2; i*j <= 30000; j++) {
+      for (j = 2; i*j <= 3000000; j++) {
         prim[i*j] = 0;
       }
     }
   }
   
-  for (i = 2; i<= 30000; i++) {
+  for (i = 2; i<= 3000000; i++) {
     if (prim[i] == 1) {
       printf("%d ", i);
     }
